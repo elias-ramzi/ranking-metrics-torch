39348aa2be4f9a107c061bcdd4ddbf7401990cf9
Elias Ramzi <elias.ramzi@gmail.com>
2021-07-15 09:54:40 +0200

    added @R option

--- ranking_metrics_torch/avg_precision.py
+++ ranking_metrics_torch/avg_precision.py
@@ -9,6 +9,7 @@ from ranking_metrics_torch.precision_recall import precision_at
 
 def avg_precision_at(
     ks: torch.Tensor, scores: torch.Tensor, labels: torch.Tensor,
+    at_R=False,
     embeddings_come_from_same_source: bool = False,
 ) -> torch.Tensor:
     """Compute average precision at K for provided cutoffs
@@ -33,7 +34,8 @@ def avg_precision_at(
     rel_precisions = precisions * topk_labels
 
     for index, k in enumerate(ks):
+        normalize = num_relevant if at_R else num_relevant.clamp(min=1, max=k)
         total_prec = rel_precisions[:, : int(k)].sum(dim=1)
-        avg_precisions[:, index] = total_prec / num_relevant.clamp(min=1, max=k)
+        avg_precisions[:, index] = total_prec / normalize
 
     return _mask_with_nans(avg_precisions, labels)
